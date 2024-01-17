9fa70252ad2e5dac39e0ad9c6f5b0a46f6fce180
John Shaffer <jdsha@pm.me>
2024-01-16 22:47:15 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# salmon.construct
+
+[![Clojars Project](https://img.shields.io/clojars/v/rs.shaffe/salmon.construct.svg)](https://clojars.org/rs.shaffe/salmon.construct)
+
+[![CljDoc Badge](https://cljdoc.org/badge/rs.shaffe/salmon.construct)](https://cljdoc.org/d/rs.shaffe/salmon.construct)
+
+*Pre-alpha* AWS constructs for [Salmon](https://github.com/john-shaffer/salmon).
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "salmon.construct";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell {
+          buildInputs = [
+            clj-kondo
+            clojure
+            git
+            jdk
+            neil
+            packer
+            python39Packages.cfn-lint
+            rlwrap # Used by clj
+            time
+          ];
+        };
+      });
+}
