0bbb1f38f98268bcc183dc9614ab697271101627
John Shaffer <jdsha@pm.me>
2025-02-10 21:19:22 -0600

    Upgrade deps

--- flake.nix
+++ flake.nix
@@ -2,13 +2,16 @@
   description = "salmon.construct";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
-      with import nixpkgs { inherit system; }; {
+      with import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      }; {
         devShells.default = mkShell {
           buildInputs = [
             clj-kondo
@@ -17,7 +20,7 @@
             jdk
             neil
             packer
-            python39Packages.cfn-lint
+            python312Packages.cfn-lint
             rlwrap # Used by clj
             time
           ];
